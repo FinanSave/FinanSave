6398e8135973c9e8fadf677a656bd82a2b2ecc22
Leonardo Viana <leonardoviana.nave@gmail.com>
2024-08-13 22:58:12 -0300

    refactor: extensions recomended and eslint fix

--- front/src/app/login/page.tsx
+++ front/src/app/login/page.tsx
@@ -1,88 +1,98 @@
-"use client";
+'use client'
 
-import React, { useState } from 'react';
-import { useRouter } from 'next/navigation';
+import React, { useState } from 'react'
+import { useRouter } from 'next/navigation'
 
 const LoginPage = () => {
   const [formData, setFormData] = useState({
     username: '',
-    password: ''
-  });
+    password: '',
+  })
 
-  const [error, setError] = useState('');
-  const router = useRouter();
+  const [error, setError] = useState('')
+  const router = useRouter()
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
+    const { name, value } = e.target
+    setFormData({ ...formData, [name]: value })
+  }
 
   const validateLogin = () => {
     // Simulação de validação. O back-end deve realizar a validação real.
-    if (formData.username !== 'usuarioValido' || formData.password !== 'senhaValida') {
-      setError('Usuário ou senha inválidos');
-      return false;
+    if (
+      formData.username !== 'usuarioValido' ||
+      formData.password !== 'senhaValida'
+    ) {
+      setError('Usuário ou senha inválidos')
+      return false
     }
-    return true;
-  };
+    return true
+  }
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+    e.preventDefault()
     if (validateLogin()) {
       // Lógica para redirecionar após login bem-sucedido
-      console.log('Login bem-sucedido');
-      router.push('/dashboard');  // Exemplo de redirecionamento
+      console.log('Login bem-sucedido')
+      router.push('/dashboard') // Exemplo de redirecionamento
     }
-  };
+  }
 
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-200">
-      <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
-        <h1 className="text-3xl font-bold text-center mb-6">Fazer login</h1>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-200">
+      <div className="w-full max-w-md rounded bg-white p-8 shadow-md">
+        <h1 className="mb-6 text-center text-3xl font-bold">Fazer login</h1>
         <form onSubmit={handleSubmit} className="space-y-4">
           <div>
-            <label htmlFor="username" className="block text-sm font-medium text-gray-700">Nome de usuário</label>
+            <label
+              htmlFor="username"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Nome de usuário ou e-mail
+            </label>
             <input
               id="username"
               name="username"
               type="text"
               required
-              className="mt-1 p-2 border border-gray-300 rounded w-full"
+              className="mt-1 w-full rounded border border-gray-300 p-2"
               value={formData.username}
               onChange={handleChange}
             />
           </div>
           <div>
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700">Senha</label>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Senha
+            </label>
             <input
               id="password"
               name="password"
               type="password"
               required
-              className="mt-1 p-2 border border-gray-300 rounded w-full"
+              className="mt-1 w-full rounded border border-gray-300 p-2"
               value={formData.password}
               onChange={handleChange}
             />
           </div>
-          {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
+          {error && <p className="mt-1 text-sm text-red-500">{error}</p>}
           <button
             type="submit"
-            className="w-full py-2 px-4 bg-blue-600 text-white font-bold rounded hover:bg-blue-700 focus:outline-none"
+            className="w-full rounded bg-blue-600 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
           >
             Entrar
           </button>
         </form>
-        <div className="text-center mt-4">
-          <a
-            href="/register"
-            className="text-blue-700 hover:underline"
-          >
+        <div className="mt-4 text-center">
+          <a href="/register" className="text-blue-700 hover:underline">
             Criar conta
           </a>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default LoginPage;
+export default LoginPage
--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,40 +1,40 @@
-"use client";
+'use client'
 
-import React from 'react';
-import { useRouter } from 'next/navigation';
+import React from 'react'
+import { useRouter } from 'next/navigation'
 
 const IndexPage = () => {
-  const router = useRouter();
+  const router = useRouter()
 
   const handleRegister = () => {
-    router.push('/register');
-  };
+    router.push('/register')
+  }
 
   return (
-    <div className="min-h-screen flex flex-col items-center bg-gray-100">
+    <div className="flex min-h-screen flex-col items-center bg-gray-100">
       {/* Header */}
       <header className="w-full bg-gray-200 py-6 shadow-md">
-        <div className="container mx-auto flex justify-between items-center px-4">
+        <div className="container mx-auto flex items-center justify-between px-4">
           <div className="flex items-center">
             <img
               src="/FS-symbol-LP.png"
               alt="Finansave Logo"
               className="h-14"
             />
-            <span className="text-4xl font-bold ml-3">
+            <span className="ml-3 text-4xl font-bold">
               Finan<span className="text-blue-700">Save</span>
             </span>
           </div>
-          <div className="flex space-x-4 items-center">
+          <div className="flex items-center space-x-4">
             <button
               onClick={handleRegister}
-              className="text-gray-700 font-semibold hover:underline py-2"
+              className="py-2 font-semibold text-gray-700 hover:underline"
             >
               Criar conta
             </button>
             <a
               href="/login"
-              className="bg-blue-700 text-white py-2 px-4 rounded hover:bg-blue-800"
+              className="rounded bg-blue-700 px-4 py-2 text-white hover:bg-blue-800"
             >
               Fazer login
             </a>
@@ -43,30 +43,28 @@ const IndexPage = () => {
       </header>
 
       {/* Main Content */}
-      <main className="flex flex-col items-start mt-8 px-4 text-left w-full max-w-7xl mx-auto">
+      <main className="mx-auto mt-8 flex w-full max-w-7xl flex-col items-start px-4 text-left">
         <h1 className="text-3xl font-bold">
           Gerencie suas finanças de forma fácil e eficiente
         </h1>
-        <p className="text-3xl font-bold">
-          com nosso site intuitivo
-        </p>
-        <p className="text-lg mt-4 max-w-xl">
-          Nossa plataforma completa para você com Controle de Gastos, 
+        <p className="text-3xl font-bold">com nosso site intuitivo</p>
+        <p className="mt-4 max-w-xl text-lg">
+          Nossa plataforma completa para você com Controle de Gastos,
           Planejamento de Orçamento e Relatórios Personalizados
         </p>
-        <div className="flex justify-start space-x-12 mt-12">
+        <div className="mt-12 flex justify-start space-x-12">
           <div className="text-center">
-            <div className="bg-gray-300 h-48 w-48 rounded-md mb-4"></div>
+            <div className="mb-4 h-48 w-48 rounded-md bg-gray-300"></div>
             <h2 className="text-xl font-bold">Promoções</h2>
           </div>
           <div className="text-center">
-            <div className="bg-gray-300 h-48 w-48 rounded-md mb-4"></div>
+            <div className="mb-4 h-48 w-48 rounded-md bg-gray-300"></div>
             <h2 className="text-xl font-bold">Benefícios</h2>
           </div>
         </div>
       </main>
     </div>
-  );
-};
+  )
+}
 
-export default IndexPage;
+export default IndexPage
